e464e98402c164a3a1251c7a6a8820fe3332c08f
Moses <matimba.moses@boxfusion.io>
2021-08-08 16:39:13 +0200

    added basic stracter

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Hello world!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<SafeAreaView style={styles.container}>
+			<HomeScreen />
+		</SafeAreaView>
 	);
 }
 
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen = () => {
+	return (
+		<View>
+			<Text>shirley</Text>
+		</View>
+	);
+};
+
+export default HomeScreen;
